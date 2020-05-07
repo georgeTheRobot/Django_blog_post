8b0385555a72f8f5dc1894b440173df04b83dcfb
georgeTheRobot <gmwangi118@yahoo.com>
2020-05-07 16:55:42 +0300

    first commit

---
+++ users/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from PIL import Image
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+	def __str__(self):
+		return f'{self.user.username} profile'
+
+#the code below is used to resize profile picture
+	def save(self, *args, **kwargs):
+		super().save()
+
+		img = Image.open(self.image.path)
+
+		if img.height > 300 or img.width > 300:
+			output_size = (300, 300)
+			img.thumbnail(output_size)
+			img.save(self.image.path)
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
